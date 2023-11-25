88c085047089f8d83592cfab17509921fafe217d
pro <quddbscjswow@naver.com>
2023-11-25 10:31:42 +0900

    python

---
+++ chapter03_02.py
@@ -0,0 +1,15 @@
+# 문자열 생성
+str1 = "I am Python"
+str2 = "Python"
+str3 = """How are you?"""
+str4 = '''Thank you'''
+
+print(type(str1), type(str2), type(str3), type(str4))
+print(len(str1), len(str2), len(str3), len(str4))
+
+# 빈 문자열
+str1_t1 = ''
+str2_t2 = str()
+
+print(type(str1_t1), len(str1_t1))
+print(type(str2_t2), len(str2_t2))
