3b17c13aefbeb85313c5562d2bdf0d412b74c478
lby132 <quddbscjswow@naver.com>
2023-11-30 00:00:53 +0900

    python

--- chapter04_02.py
+++ chapter04_02.py
@@ -32,3 +32,88 @@ names = ['Kim', 'Park', 'Cho', 'Lee', 'Choi', 'Yoo']
 
 for n in names:
     print('You are : ', n)
+print()
+
+# 예제2
+lotto_numbers = [11, 19, 21, 28, 36, 37]
+
+for n in lotto_numbers:
+    print('Current number :', n)
+print()
+
+
+# 예제3
+word = "Beautiful"
+
+for s in word:
+    print('word : ', s)
+print()
+
+
+# 예제4
+my_info = {
+    "name": "Lee",
+    "Age": 33,
+    "City": "Seoul"
+}
+
+for k in my_info:
+    print('key', k)
+    # print('value', my_info[k])
+
+for v in my_info.values():
+    print('value :', v)
+print()
+
+# 예제4
+name = 'FineAppLE'
+
+for n in name:
+    if n.isupper():
+        print(n)
+    else:
+        print(n.upper())
+
+# break
+
+numbers = [14, 3, 4, 7, 10, 24, 17, 2, 33, 15, 34, 36, 38]
+
+for n in numbers:
+    if n == 34:
+        print('Found :', n)
+        break
+    else:
+        print('Not found : ', n)
+
+# continue
+lt = ["1", 2, 5, True, 4.3, complex(4)]
+
+for v in lt:
+    if type(v) is bool:
+        continue
+    print('current type:', v, type(v))
+    print('multiply by 2', v * 3)
+
+# for - else
+numbers = [14, 3, 4, 7, 10, 24, 17, 2, 33, 15, 34, 36, 38]
+
+for num in numbers:
+    if num == 55:
+        print("Found : 24")
+        break
+else:
+    print('Not Found : 24')
+
+# 구구단 출력
+
+for i in range(2, 10):
+    for j in range(1, 10):
+        print('{:4d}'.format(i*j), end = '')
+    print()
+
+# 변환 예제
+name2 = 'Aceman'
+print('Reversed', reversed(name2))
+print('List', list(reversed(name2)))
+print('Tuple', tuple(reversed(name2)))
+print('Set', set(reversed(name2)))
