2721abb9f56aab2f7ddcc43f3d994c0dd94027b1
lby132 <quddbscjswow@naver.com>
2023-12-04 15:26:41 +0900

    python

--- chapter06_02.py
+++ chapter06_02.py
@@ -17,11 +17,25 @@ def power(x, y):
     return x ** y
 
 
-print('-' * 15)
-print('called! inner!')
-print(add(5,5))
-print(subtract(15,5))
-print(multiply(5,5))
-print(divide(10,2))
-print(power(5,3))
-print('-' * 15)
+# print('-' * 15)
+# print('called! inner!')
+# print(add(5,5))
+# print(subtract(15,5))
+# print(multiply(5,5))
+# print(divide(10,2))
+# print(power(5,3))
+# print('-' * 15)
+
+
+# __name__ 사용
+# 내 자신을 바로 실행할때 __main__ 이 실행된다.
+# 이코드는 외부에서 import를 해서 자동으로 실행되지 않게 해주는 코드이다.
+if __name__ == "__main__":
+    print('-' * 15)
+    print('called! __main__!')
+    print(add(5, 5))
+    print(subtract(15, 5))
+    print(multiply(5, 5))
+    print(divide(10, 2))
+    print(power(5, 3))
+    print('-' * 15)
--- module_test.py
+++ module_test.py
@@ -7,8 +7,16 @@ print(sys.path)
 print(type(sys.path))
 
 # 모듈 경로 삽입
-sys.path.append('/Users/lby/PycharmProjects/pythonProject/MathTest')
+sys.path.append("/Users/lby/PycharmProjects/MathTest")
 # 모듈 경로 삽입 후
 print(sys.path)
 
 # import test_module
+
+# 모듈 사용
+# print(test_module.power(10, 3))
+
+import chapter06_02
+
+# chapter06_02 의 코드가 자동으로 사용되지 않고 이렇게 호출해야만 사용할 수 있게 만들었다.
+print(chapter06_02.add(10, 100000))
