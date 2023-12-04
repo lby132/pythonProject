dea6047a39b3d171267b2ef5d925b4d5023d9372
lby132 <quddbscjswow@naver.com>
2023-12-04 18:01:09 +0900

    python

--- module_test.py
+++ module_test.py
@@ -16,7 +16,7 @@ print(sys.path)
 # 모듈 사용
 # print(test_module.power(10, 3))
 
-import chapter06_02
+from sub.sub3 import chapter06_02
 
 # chapter06_02 의 코드가 자동으로 사용되지 않고 이렇게 호출해야만 사용할 수 있게 만들었다.
 print(chapter06_02.add(10, 100000))
