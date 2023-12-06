c240b03294d1aff271c8595cec66aa736e432834
lby132 <quddbscjswow@naver.com>
2023-12-06 22:19:34 +0900

    python

--- chapter08_01.py
+++ chapter08_01.py
@@ -48,3 +48,36 @@ def conv_pos(x):
 print(list(map(conv_pos, [1, -3, 2, 0, -5, 6])))
 print(list(map(lambda x: abs(x), [1, -3, 2, 0, -5, 6])))
 
+# pow : 제곱값 반환
+print(pow(2,10))
+
+# range : 반복가능한 객체(Iterable) 반환
+print(range(1,10,2))
+print(list(range(1,10,2)))
+print(list(range(0,-15,-1)))
+
+
+# round : 반올림
+print(round(6.5781,2))
+print(round(5.6))
+
+# sorted : 반복가능한 객체(Iterable) 정렬 후 반환
+print(sorted([6,7,4,3,1,2]))
+a = sorted([6,7,4,3,1,2])
+print(a)
+print(sorted(['p','y','t','h','o','n']))
+
+
+# sum : 반복가능한 객체(Iterable) 합 반환
+print(sum([6,7,4,3,1,2]))
+print(sum(range(1,101)))
+
+# type : 자료형 확인
+print(type(3))
+print(type({}))
+print(type(()))
+print(type([]))
+
+# zip : 반복가능한 객체(Iterable)의 요소를 묶어서 반환
+print(list(zip([10,20,30],[40,50,60])))
+print(type(list(zip([10,20,30],[40,50,60]))[0]))
